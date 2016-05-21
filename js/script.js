29b53163d2a78e7d0d8dc514c7507f6d4df832a4
yayimahuman <donha@utschools.ca>
2016-05-21 18:29:40 -0400

    added stuff

---
+++ js/script.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function () {
+    var map;
+    var style =
+[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
+    var options = {
+        zoom: 11,
+        center:  new google.maps.LatLng(43.7000, -79.4000),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+    map = new google.maps.Map($('#map')[0], options);
+    map.setOptions({
+        styles: style
+    });
+});
+$("#form-group-3 button").on("click",function(){
+    submit();
+});
+
+function submit(){
+    event.preventDefault();
+    console.log("hello");
+        origin = $("input#origin").val();
+        destination = $("input#destination").val();
+        $('#menu').remove();
+        console.log("origin is " + origin +  " and destination is " + destination);
+                //sidd's code
+        //var origin = new google.maps.LatLng(55.930385, -3.118425),
+        //var origin = "Toronto, ON",
+            //destination = "Pickering, ON",
+}
