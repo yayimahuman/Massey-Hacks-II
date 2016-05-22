0365c026fb936cb75371ee19eef78d3b7f4bfa3b
yayimahuman <donha@utschools.ca>
2016-05-22 05:31:06 -0400

    done
    added things

--- js/script.js
+++ js/script.js
@@ -1,45 +1,219 @@
-// Google Maps Scripts
-var map = null;
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(43.7181552, -79.5184866));
+var x = "Fatima";
+status = 0;
+$(document).ready(function(){
+    $("#welcome").html("Welcome, " + x + "!");
+    $("#profile").hide();
+    $("#meals").hide();
+    $("#carbon").hide();
+    $("#new").hide();
+
 });
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 12,
+function profile(){
+    $("#home").fadeOut();
+    $("#profile").fadeIn();
+    $("#add-img").attr("src", "css/img/arrow.png");
+    status = 1;
+}
+function meals(){
+    $("#home").fadeOut();
+    $("#meals").fadeIn();
+    $("#add-img").attr("src", "css/img/arrow.png");
+    status = 1;
+}
+function carbon(){
+    $("#home").fadeOut();
+    $("#carbon").fadeIn();
+    $("#add-img").attr("src", "css/img/arrow.png");
+    status = 1;
+}
+function add(){
+    if (status == 0){
+        $("#home").fadeOut();
+        $("#new").fadeIn();
+        $("#list").fadeIn();
+        $("#add-img").attr("src", "css/img/arrow.png");
+        status = 1;
+    }
+    else if (status == 2){
+        $("#list").html("");
+        $("#home").fadeIn();
+        $("#new").fadeOut();
+        $("#add-img").attr("src", "css/img/plus.png");
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(43.6532, -79.3832), // New York
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: false,
-        scrollwheel: false,
-        draggable: true,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles:
-            [{"featureType":"water","elementType":"all","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"on"}]},{"featureType":"water","elementType":"labels","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"hue":"#83cead"},{"saturation":1},{"lightness":-15},{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"hue":"#f3f4f4"},{"saturation":-84},{"lightness":59},{"visibility":"on"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbbbbb"},{"saturation":-100},{"lightness":26},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-35},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-22},{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"hue":"#d7e4e4"},{"saturation":-60},{"lightness":23},{"visibility":"on"}]}]
-    };
+        status = 0;
+    }
+    else{
+        $("#home").fadeIn();
+        $("#profile").fadeOut();
+        $("#meals").fadeOut();
+        $("#carbon").fadeOut();
+        $("#new").fadeOut();
+        $("#add-img").attr("src", "css/img/plus.png");
+        status = 0;
+    }
+}
+counter = 0;
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+function addToList(){
+    //.hide().fadeIn();
+    food = $("#food").val();
+    console.log(food);
+    $("#food").val("");
+    var imageId = "img" + counter;
+    console.log(imageId);
+    var code = '<div class="col-sm-4"><img id="'+imageId+'" class="img-responsive" src="css/img/' + food + '.jpg" /></div>';
+    console.log(code);
+    $("#list").append(code);
+    $("#"+imageId).hide().fadeIn();
+    var y = checkFood();
+    counter++;
+    status = 2;
+}
 
-    // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+function checkFood(){
+    var cal;
+    switch(food){
+    	case "Milk":
+    	     cal = 18500;
+    	     break;
+    	case "Orange":
+    	     cal = 190;
+    	     break;
+    	case "Apple":
+    	     cal = 190;
+    	     break;
+    	case "Soy":
+    	     cal = 990;
+    	     break;
+    	case "Grape":
+    	     cal = 500;
+    	     break;
+    	case "Beef":
+    	     cal = 27000;
+    	     break;
+    	case "Sausage":
+    	     cal = 8000;
+    	     break;
+    	case "Pork":
+    	     cal = 12100;
+    	     break;
+    	case "Ham":
+    	     cal = 4800;
+    	     break;
+    	case "Poultry":
+    	     cal = 3500;
+    	     break;
+    	case "Butter":
+    	     cal = 23800;
+    	     break;
+    	case "Cheese":
+    	     cal = 13500;
+    	     break;
+    	case "Cream":
+    	     cal = 7600;
+    	     break;
+    	case "Egg":
+    	     cal = 1950;
+    	     break;
+    	case "Quard":
+    	     cal = 1950;
+    	     break;
+    	case "Curd":
+    	     cal = 1950;
+    	     break;
+    	case "Margarine":
+    	     cal = 1350;
+    	     break;
+    	case "Yogurt":
+    	     cal = 1250;
+    	     break;
+    	case "Strawberry":
+    	     cal = 300;
+    	     break;
+    	case "Bread":
+    	     cal = 750;
+    	     break;
+    	case "Lamb":
+    	     cal = 39200;
+    	     break;
+    	case "Turkey":
+    	     cal = 10900;
+    	     break;
+    	case "Chicken":
+    	     cal = 6900;
+    	     break;
+    	case "Tuna":
+    	     cal = 6100;
+    	     break;
+    	case "Potato":
+    	     cal = 2900;
+    	     break;
+    	case "Rice":
+    	     cal = 2700;
+    	     break;
+    	case "Nut":
+    	     cal = 2300;
+    	     break;
+    	case "Tofu":
+    	     cal = 2000;
+    	     break;
+    	case "Lettuce":
+    	     cal = 2000;
+    	     break;
+    	case "Lentil":
+    	     cal = 900;
+    	     break;
+    	case "Banana":
+    	     cal = 480;
+    	     break;
+    	case "Latte":
+    	     cal = 340;
+    	     break;
+    	case "Tea":
+    	     cal = 60;
+    	     break;
+    	case "Coffee":
+    	     cal = 60;
+    	     break;
+    	case "Beer":
+    	     cal = 900;
+    	     break;
+    	case "Salmon":
+    	     cal = 11900;
+    	     break;
+    	case "Yogurt":
+    	     cal = 2200;
+    	     break;
+    	case "Broccoli":
+    	     cal = 2000;
+    	     break;
+    	case "Beans":
+    	     cal = 2000;
+    	     break;
+    	case "Tomatoes":
+    	     cal = 1100;
+    	     break;
+    	case "Cereal":
+    	     cal = 3000;
+    	     break;
+    	case "Oil":
+    	     cal = 2000;
+    	     break;
+    	case "Sugar":
+    	     cal = 2000;
+    	     break;
+    	case "Ketchup":
+    	     cal = 2000;
+    	     break;
+    	case "Chips":
+    	     cal = 2200;
+    	     break;
+    	 default:
+    	 	cal = 0
+    }
+    console.log(cal);
+    return cal;
 }
